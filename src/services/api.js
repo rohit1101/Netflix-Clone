e78b025658ff9b8cd011d976c554f0ca7e4e1908
rohit1101 <srohit1101@gmail.com>
2021-07-21 11:44:01 +0530

    env bug fixed

--- src/services/api.js
+++ src/services/api.js
@@ -2,28 +2,28 @@ import { baseURL } from "../constants";
 
 export async function homeCall() {
   const response = await fetch(
-    `${baseURL}/trending/all/week?api_key=${process.env.production.REACT_APP_API_KEY}`
+    `${baseURL}/trending/all/week?api_key=${process.env.REACT_APP_API_KEY}`
   );
   return await response.json();
 }
 
 export async function tvCall() {
   const response = await fetch(
-    `${baseURL}/tv/popular?api_key=${process.env.production.REACT_APP_API_KEY}&language=en-US&page=1`
+    `${baseURL}/tv/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`
   );
   return await response.json();
 }
 
 export async function moviesCall() {
   const response = await fetch(
-    `${baseURL}/movie/popular?api_key=${process.env.production.REACT_APP_API_KEY}&language=en-US&page=2`
+    `${baseURL}/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=2`
   );
   return await response.json();
 }
 
 export async function newAndPopularCall() {
   const response = await fetch(
-    `${baseURL}/trending/all/day?api_key=${process.env.production.REACT_APP_API_KEY}&language=en-US&page=10`
+    `${baseURL}/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=10`
   );
   return await response.json();
 }
