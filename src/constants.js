479bd28ee588d4ef48d9ed554d9ae8da3805a4f9
rohit1101 <srohit1101@gmail.com>
2021-07-21 11:35:19 +0530

    update env

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,4 @@
 export const baseURL = `https://api.themoviedb.org/3`;
-export const key = `api_key=${process.env.REACT_APP_API_KEY}`;
 export const imagePath = `https://image.tmdb.org/t/p`;
 
 export const backDropSize = {
--- src/services/api.js
+++ src/services/api.js
@@ -1,27 +1,29 @@
-import { baseURL, key } from "../constants";
+import { baseURL } from "../constants";
 
 export async function homeCall() {
-  const response = await fetch(`${baseURL}/trending/all/week?${key}`);
+  const response = await fetch(
+    `${baseURL}/trending/all/week?api_key=${process.env.REACT_APP_API_KEY}`
+  );
   return await response.json();
 }
 
 export async function tvCall() {
   const response = await fetch(
-    `${baseURL}/tv/popular?${key}&language=en-US&page=1`
+    `${baseURL}/tv/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`
   );
   return await response.json();
 }
 
 export async function moviesCall() {
   const response = await fetch(
-    `${baseURL}/movie/popular?${key}&language=en-US&page=2`
+    `${baseURL}/movie/popular?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=2`
   );
   return await response.json();
 }
 
 export async function newAndPopularCall() {
   const response = await fetch(
-    `${baseURL}/trending/all/day?${key}&language=en-US&page=10`
+    `${baseURL}/trending/all/day?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=10`
   );
   return await response.json();
 }
